c0f5d227e24affcb643ecb170e957ba1424bc947
jun <dongrim318@gmail.com>
2020-08-19 22:15:58 +0900

    initial commit

---
+++ javascript/hoisting.js
@@ -0,0 +1,21 @@
+
+function countWithVar () {
+  for (var i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000)
+  }
+}
+countWithVar();
+// 결과: 5, 5, 5, 5, 5
+
+
+function countWithLet () {
+    for (let i = 0; i < 5; i++) {
+      setTimeout(function () {
+          console.log(i);
+      }, 1000)
+    }
+  }
+  countWithLet();
+  //결과 0, 1, 2, 3, 4
